94d28c01f2a0d589afee2cce022b96a9addb4192
bjekovicn <n.bjekovic97@gmail.com>
2024-05-14 15:05:07 +0200

    localization footer

--- src/common/components/footer.tsx
+++ src/common/components/footer.tsx
@@ -1,7 +1,10 @@
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 import Logo from "./logo";
+import { useTranslation } from "react-i18next";
 
 function Footer() {
+  const { t } = useTranslation();
+
   return (
     <div className="flex w-full mt-10 h-96 flex-col">
       <div className="w-full flex flex-col md:flex-row h-full bg-gray-950 justify-center items-center gap-2">
@@ -9,24 +12,19 @@ function Footer() {
           <Logo />
         </div>
         <p className="text-white text-xs md:text-sm lg:text-lg text-center flex-1 items-center justify-center mx-8">
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed.
+          {t("footerQuote")}
         </p>
         <div className="flex flex-1 justify-center items-center flex-col">
           <div>
             <input
-              defaultValue={"Enter your email address"}
+              placeholder={t("enterEmail")}
               className="text-black px-4 py-2 mr-3"
             ></input>
             <button className="text-white px-4 bg-pink-600 py-2">
-              Subscribe
+              {t("subscribe")}
             </button>
           </div>
-          <p className="text-white mt-2">
-            By subscribing you agree to our Privacy Policy
-          </p>
+          <p className="text-white mt-2">{t("subscribeWarning")}</p>
         </div>
       </div>
       <div className="w-full py-4 gap-8 flex flex-row h-14 bg-gray-950 justify-center items-center">
