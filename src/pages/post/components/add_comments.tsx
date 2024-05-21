caac2be52f21e538b0d67b27668ee5fb0fc90f2e
bjekovicn <n.bjekovic97@gmail.com>
2024-05-21 10:48:17 +0200

    izmjene

--- src/pages/post/components/add_comments.tsx
+++ src/pages/post/components/add_comments.tsx
@@ -33,7 +33,7 @@ const AddComment: React.FC<{
       <div className="border-gray-100 border-solid border-2 p-2 gap-2 flex flex-col items-center">
         <input
           placeholder={t("addComment.name")}
-          className="text-black px-4 py-2  border border-gray-200 border-solid w-full"
+          className="text-black py-0 px-4 py-2  border border-gray-200 border-solid w-full"
           {...register("name")}
         />
         {errors.name && <span>{errors.name.message}</span>}
