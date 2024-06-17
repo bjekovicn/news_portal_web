d2b75c40b1b40b90da38210032a074a35582239d
bjekovicn <n.bjekovic97@gmail.com>
2024-06-17 09:14:34 +0200

    added slug

--- src/schemas/post-details-schema.ts
+++ src/schemas/post-details-schema.ts
@@ -1,21 +1,16 @@
 import { z } from "zod";
 import { CoverMediaSchema } from "./cover-media-schema";
-import { CategorySchema } from "./category-schema";
+import { ReducedCategorySchema } from "./category-schema";
 
 export const PostDetailsSchema = z.object({
   id: z.number(),
-  attributes: z.object({
-    title: z.string(),
-    shortSummary: z.string(),
-    createdAt: z.string(),
-    updatedAt: z.string(),
-    content: z.any(),
-    categories: z.object({
-      data: z.array(CategorySchema),
-    }),
-    coverMedia: z.object({
-      data: z.object({ attributes: CoverMediaSchema.nullable() }).nullable(),
-    }),
-    likes: z.number().nullable(),
-  }),
+  title: z.string(),
+  shortSummary: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  content: z.any(),
+  categories: z.array(ReducedCategorySchema),
+
+  coverMedia: CoverMediaSchema.nullable(),
+  likes: z.number().nullable(),
 });
